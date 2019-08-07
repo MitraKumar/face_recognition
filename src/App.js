dcef649bff16309bb066384a4ebcffa315fa0ec5
MitraKumar <kaushikmitra556@gmail.com>
2019-08-08 01:12:18 +0530

    Updated App.js to fix an error

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { Route, Router } from 'react-router-dom';
-import createHistory from 'history/createBrowserHistory';
+import { createBrowserHistory } from 'history';
 import './App.css';
 
 import Home from './views/Home';
@@ -11,7 +11,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <Router history={createHistory()}>
+        <Router history={createBrowserHistory()}>
           <div className="route">
             <Route exact path="/" component={Home} />
             <Route exact path="/photo" component={ImageInput} />
